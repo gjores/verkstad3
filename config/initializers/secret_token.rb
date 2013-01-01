f8ac9704d7dc393da206b9452d0d7cace07394bd
Petter Gjöres <gjores@gmail.com>
2013-01-01 16:53:39 +0100

    first vommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Verkstad3::Application.config.secret_token = '88aa0f7285b0a12695678dda28f8e257fac846b3f557a87b1d0df8bc7940ff6d9f24320b563a16fbebaff70ce86387a46d08380d7d46088833019063f7edd9be'
